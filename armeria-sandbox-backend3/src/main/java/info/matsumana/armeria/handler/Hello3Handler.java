f05d977f824d69f60f746e08c3bf62b3b0a0324c
Manabu Matsuzaki <matsumana@gmail.com>
2019-07-11 02:45:55 +0900

    Fix Circuit Breaker fallback

--- armeria-sandbox-backend3/src/main/java/info/matsumana/armeria/handler/Hello3Handler.java
+++ armeria-sandbox-backend3/src/main/java/info/matsumana/armeria/handler/Hello3Handler.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import com.linecorp.armeria.client.circuitbreaker.FailFastException;
+
 import hu.akarnokd.rxjava2.interop.SingleInterop;
 import info.matsumana.armeria.retrofit.HelloClient;
 import info.matsumana.armeria.thrift.Hello3Service;
@@ -27,6 +29,14 @@ public class Hello3Handler implements Hello3Service.AsyncIface {
         final HelloClient helloClient = retrofit.create(HelloClient.class);
         SingleInterop.fromFuture(helloClient.hello(name))
                      .doOnSuccess(res -> log.debug("Retrofit HelloClient res={}", res))
+                     .doOnError(e -> log.debug("Retrofit HelloClient exception", e))
+                     .onErrorReturn(e -> {
+                         if (e instanceof FailFastException || e.getCause() instanceof FailFastException) {
+                             // Circuit Breaker fallback
+                             return "[backend4 - fallback] Hello, ???";
+                         }
+                         throw new RuntimeException(e);
+                     })
                      .map(s -> s + " & " + "[backend3] Hello, " + name)
                      .subscribe(resultHandler::onComplete,
                                 e -> resultHandler.onError(new Exception(e)));
