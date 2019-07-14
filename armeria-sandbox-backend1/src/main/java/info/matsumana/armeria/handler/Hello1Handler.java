fe888e95023987713ef2140e98aa856d38d9d2da
Manabu Matsuzaki <matsumana@gmail.com>
2019-07-15 00:26:38 +0900

    Delete unnecessary logics

--- armeria-sandbox-backend1/src/main/java/info/matsumana/armeria/handler/Hello1Handler.java
+++ armeria-sandbox-backend1/src/main/java/info/matsumana/armeria/handler/Hello1Handler.java
@@ -9,23 +9,8 @@ import info.matsumana.armeria.thrift.Hello1Service;
 @Component
 public class Hello1Handler implements Hello1Service.AsyncIface {
 
-    private final FailHandler failHandler;
-
-    Hello1Handler(FailHandler failHandler) {
-        this.failHandler = failHandler;
-    }
-
     @Override
     public void hello(String name, AsyncMethodCallback<String> resultHandler) throws TException {
-        // Just test for Circuit Breaker
-        testCircuitBreaker();
-
         resultHandler.onComplete("[backend1] Hello, " + name);
-    }
-
-    private void testCircuitBreaker() {
-        if (failHandler.isFail()) {
-            throw new RuntimeException("Circuit breaker test");
-        }
     }
 }
